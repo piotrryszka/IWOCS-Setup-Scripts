4329fad9584fe8f602ed176f2b3d9d1d7d03c55e
Mikołaj Sztaba <mikolaj.sztaba@technipfmc.com>
2022-02-15 13:33:58 +0100

    Changes in SSH connection

--- lib/network.py
+++ lib/network.py
@@ -10,35 +10,38 @@ from datetime import datetime
 def ssh_con(file, host):
     # creating time stamp
     exact_time = datetime.date(datetime.now())
+
+    # configuration of network device
     cisco1 = {
         "device_type": f"cisco_ios",
-#         "device_type": f"autodetect",
         "host": f"{host}",
         "username": f"{username}",
         "password": f'{password}',
-#         "fast_cli": False,
-        # logger
-        "session_log": f"device-logs/{host}---{exact_time}.txt",
+        # session logger
+        "session_log": f"device-logs/ip: {host}|---|date: {exact_time}.txt",
     }
 
     try:
         with ConnectHandler(**cisco1) as net_connect:
 
-            # commands to be sent
+            # TFTP configuration download command
             our_command = f'copy tftp://{server_ip}/{file} startup-config'
-            print(our_command)
 
-            # TODO: ONE COMMAND WITH TFTP COPY
-            output = net_connect.send_command_timing(our_command, cmd_verify=False)
+            # ONE COMMAND WITH TFTP COPY
+            net_connect.send_command_timing(our_command, cmd_verify=False)
+
+            # TODO: check in LAB if it is needed to wait here
+            # waiting for tftp downloaded
             sleep(2)
 
-            # printing for tests
-            output = net_connect.send_command_timing('\n', cmd_verify=False)
-            print(output)
+            # sending confirmation of tftp copy
+            net_connect.send_command_timing('\n', cmd_verify=False)
 
-            # printing for tests
-            output = net_connect.send_command_timing('reload', cmd_verify=False)
-            print(output)
+            # command for reloading
+            net_connect.send_command_timing('reload', cmd_verify=False)
+
+            # sending confirmation of reloading copy
+            net_connect.send_command_timing('\n', cmd_verify=False)
 
     # error handling while ssh connection
     except (NetmikoTimeoutException, NetmikoAuthenticationException) as error:
--- testing/test.py
+++ testing/test.py
@@ -1,7 +1,14 @@
-from lib.network import ssh_con
-#from netmiko import ConnectHandler, NetmikoTimeoutException, NetmikoAuthenticationException
-from lib.data import password, username, decorator_1
+# from lib.network import ssh_con
+# #from netmiko import ConnectHandler, NetmikoTimeoutException, NetmikoAuthenticationException
+# from lib.data import password, username, decorator_1
+#
+#
+# new_host = '172.30.100.10'
+# ssh_con(file='TDS-1_A_test.txt', host =new_host)
 
-
-new_host = '172.30.100.10'
-ssh_con(file='TDS-1_A_test.txt', host =new_host)
+#
+# import datetime
+#
+# x = datetime.datetime(2)
+#
+# print(x.strftime("%B"))
