fa4ae45d0644faab47e9d6ac677525222075c6c6
Mikołaj Sztaba <mikolaj.sztaba@technipfmc.com>
2022-02-21 14:27:46 +0100

    Corrections in script workflow

--- testing/test.py
+++ testing/test.py
@@ -5,17 +5,3 @@
 #
 # new_host = '172.30.100.10'
 # ssh_con(file='TDS-1_A_test.txt', host =new_host)
-
-# creating list with files
-from os import listdir
-from os.path import isfile, join
-
-def listing_conf(us_choice):
-    onlyfiles = [f for f in listdir('../tftp-conf-files') if isfile(join('../tftp-conf-files', f))]
-    if us_choice in onlyfiles:
-        return True
-    else:
-        return False
-
-e = test('ETAS-1')
-print(e)
