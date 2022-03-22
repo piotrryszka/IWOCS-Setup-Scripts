8a830540dd52f9e5c6eb79028d9976e7b069efa2
Mikołaj Sztaba <mikolaj.sztaba@technipfmc.com>
2022-03-22 16:31:18 +0100

    Catching UDI and Status of license

--- testing/test.py
+++ testing/test.py
@@ -2,7 +2,20 @@
 from lib.commands import send_to_console
 from serial import Serial
 
+# TODO: ustawienie wartosci domyslnych, jezeli cos sie nie wczyta to dawanie jakies 'UNKNOWN' i uzytkownik musi srpawdzic sam
+# TODO: zrobic duza funkcje z tym wszystkim zeby wszystko zwracala
+# TODO; pozmieniac nazwy zmiennych bo tu syf jest
+
 ser = Serial('COM6', 9600)
+e = send_to_console(ser, '\n')
+e = send_to_console(ser, '\n')
+e = send_to_console(ser, '\n')
+e = send_to_console(ser, 'en')
+
+
+
+
+
 e = send_to_console(ser, 'sh license udi', 0.5)
 
 print(e)
@@ -15,6 +28,7 @@ udi = li[11]
 # UDI
 print(udi)
 
+# sprawdzanie licencji oraz jej stanu
 e = send_to_console(ser, 'sh license', 2)
 
 
@@ -23,18 +37,48 @@ with open("data.txt", "w") as text_file:
 
 list_of_lists = []
 
+# flags to leave script properly
+flag_1 = False
+flag_2 = False
 
 with open('data.txt') as f:
-    for line in f:
-        data = f.read()
-        print(data)
+    counter =0
+    lines = f.readlines()
+    for line in lines:
+        print(line)
+        if counter < 1:
+            if 'License State:' in line:
+                e = line.split()
+                our_status = e[2:]
+                print(our_status)
+                my_string = ' '.join(our_status)
+                # tego my stringa wsadzic w pole STATUS
+                print(my_string)
+                flag_1 = True
+            if 'License Type:' in line:
+                e = line.split()
+                our_status = e[2:]
+                print(our_status)
+                my_string = ' '.join(our_status)
+                # tego my stringa wsadzic w pole EXPIRATION
+                print(my_string)
+
+        if 'ipservices' in line:
+            print("ESSA")
+        if 'lanbase' in line:
+            counter =+1
+
+
+
+#         data = f.read()
+#         print(data)
 
 
 
 # print(data)
 
-content_list = data.split()
-print(content_list)
+# content_list = data.split()
+# print(content_list)
 # li = list(e.split())
 # # e.split('')
 # print(li)
