a6fc695fe2f19c7ed4ab4130a24ab91fa7b16f58
Mikołaj Sztaba <mikolaj.sztaba@technipfmc.com>
2022-03-22 14:50:16 +0100

    Trying to achieve by console info about license and udi

--- testing/test.py
+++ testing/test.py
@@ -1,2 +1,40 @@
 # TESTING
+from lib.commands import send_to_console
+from serial import Serial
 
+ser = Serial('COM6', 9600)
+e = send_to_console(ser, 'sh license udi', 0.5)
+
+print(e)
+
+li = list(e.split())
+# e.split('')
+print(li)
+udi = li[11]
+
+# UDI
+print(udi)
+
+e = send_to_console(ser, 'sh license', 2)
+
+
+with open("data.txt", "w") as text_file:
+    text_file.write(e)
+
+list_of_lists = []
+
+
+with open('data.txt') as f:
+    for line in f:
+        data = f.read()
+        print(data)
+
+
+
+# print(data)
+
+content_list = data.split()
+print(content_list)
+# li = list(e.split())
+# # e.split('')
+# print(li)
