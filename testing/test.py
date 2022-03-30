393674fd2fe27a014ea4ddfc86bb771b19b1b8af
Mikołaj Sztaba <mikolaj.sztaba@technipfmc.com>
2022-03-30 15:21:15 +0200

    Added checking device model and creating files and printing with pretty tables

--- testing/test.py
+++ testing/test.py
@@ -1,18 +1,19 @@
 from serial import Serial
 from lib.commands import send_to_console
+from prettytable import PrettyTable
 
 ser = Serial('COM4', 9600)
 
-
 output = send_to_console(ser, 'sh version', 2)
-print(output)
-print('\n')
+# print(output)
+# print('\n')
 
 with open('info.txt', 'w') as file:
     file.write(output)
 
 with open('info.txt', 'r') as my_file:
     for line in my_file:
+        # looking for actual software version
         if 'Version' in line:
             my_list = line.split(',')
             for x in my_list:
@@ -21,9 +22,27 @@ with open('info.txt', 'r') as my_file:
                     x = x.split(' ')
                     # printing version
                     print("ACTUAL VERSION")
-                    print(x[-1])
+                    act_version = x[-1]
+                    print(act_version)
+        # looking for actual device model
+        if 'System image file' in line:
+            my_list = line.split(' ')
+            for x in my_list:
+                if 'flash' in x:
+                    x = x.split('/')
+                    x = x[1].split('-')
+                    # printing device model
+                    final_device = x[0]
+                    print('DEVICE MODEL')
+                    print(final_device)
+                    print('\n')
 
+# PRETTY TABLE MOMENT
+x = PrettyTable()
+x.field_names = ["ID", "Name", "Model", "Current Version", "New Version"]
+x.add_row(['1', "TDS-1_A", f"{final_device}", f'{act_version}', 'Newcos'])
+print(x)
 
-
-#     lines = my_file.readlines()
-#     print(lines)
+# writing table to txt file
+with open('table_version.txt', 'w') as file:
+    file.write(str(x))
