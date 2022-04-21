3b1238a511ca9b2d4e1ce742c2e40729a5058d72
Mikołaj Sztaba <mikolaj.sztaba@technipfmc.com>
2022-04-21 09:07:22 +0200

    Testing script

--- main.py
+++ main.py
@@ -174,22 +174,23 @@ while running_flag:
                         except:
                             pass
 
+                        # TODO: testing needs to be uncommented
 
                         # setting COM connection
-                        ser = Serial(COM_string, COM_speed)
+                        # ser = Serial(COM_string, COM_speed)
 
                         # waiting for router/switch to boot
-                        user_boot_flag = checking_booting(port = ser)
+                        # user_boot_flag = checking_booting(port = ser)
 
                         # counting number of gigabit and fast ports
-                        device_ports = checking_switch_ports(ser_port = ser)
+                        # device_ports = checking_switch_ports(ser_port = ser)
 
                         # checking if device is really the device, which was wanted by user
-                        proper_device = checking_device(ser_port = ser, user_device = user_device, lang_dict = lang_expressions)
+                        # proper_device = checking_device(ser_port = ser, user_device = user_device, lang_dict = lang_expressions)
 
                         # returning next ip number and full name of configured device to download to specified device
-                        our_conf = creating_proper_configuration(user_device = user_device, port_num = device_ports['Gigabit'], ip_add = ip_number)
-                        # our_conf = creating_proper_configuration(user_device = user_device, port_num = 1, ip_add = ip_number)
+                        # our_conf = creating_proper_configuration(user_device = user_device, port_num = device_ports['Gigabit'], ip_add = ip_number)
+                        our_conf = creating_proper_configuration(user_device = user_device, port_num = 1, ip_add = ip_number)
 
                         # remembering old IP number, last octet is important to save to txt file
                         ip_save = ip_number
@@ -202,13 +203,17 @@ while running_flag:
                         with open('temp/ip_number.txt', 'w') as f:
                             f.write(str(ip_number))
 
+                        # TODO: DELETE IT
+                        user_boot_flag = True
+                        proper_device = True
+
                         if user_boot_flag and proper_device:
 
                             print(lang_expressions['not_configured'])
                             print(decorator_1)
 
                             # going to configuration mode
-                            to_conf_mode(ser)
+                            # to_conf_mode(ser)
 
                             # opening file with configuration
                             actual_device = actual_device
@@ -216,7 +221,7 @@ while running_flag:
 
                             # executing commands from the list
                             for command in stripped_list:
-                                send_to_console(ser, command)
+                                # send_to_console(ser, command)
                                 # printing dots to inform user that script is still working
                                 print('.', end='')
 
@@ -225,8 +230,8 @@ while running_flag:
                             # TODO: UNCOMMENT IT
                             # checking info about license on the device
                             # returning tuple with our data
-                            license_data = download_license(ser)
-                            # license_data = download_license()
+                            # license_data = download_license(ser)
+                            license_data = download_license()
 
                             # TODO: UNCOMMENT
                             # reading license data to variables use to fill txt file
@@ -252,7 +257,7 @@ while running_flag:
                                 pass
 
                             # sending command to switch with sh version
-                            sh_version(ser)
+                            # sh_version(ser)
 
                             # saving prepared data to txt, later will be prepared table report with it
                             # TUTAJ ROBOTA
@@ -267,9 +272,9 @@ while running_flag:
                                 f.write(str(id_number))
 
                             # closing connection
-                            ser.close()
+                            # ser.close()
                             print(f"{lang_expressions['proper_conf']}{user_device}.")
-                            print(f"{lang_expressions['close_con']}{ser.name}.")
+                            # print(f"{lang_expressions['close_con']}{ser.name}.")
                             print(decorator_1)
                             print('---------------------------------------------------')
                             print(decorator_1)
@@ -294,8 +299,8 @@ while running_flag:
                             print(decorator_1)
 
                             # closing connection
-                            ser.close()
-                            print(f"{lang_expressions['close_con']}{ser.name}.")
+                            # ser.close()
+                            # print(f"{lang_expressions['close_con']}{ser.name}.")
                             print(decorator_1)
                             break
                             # TODO: UNCOMMENT
@@ -326,7 +331,6 @@ while running_flag:
             # saving already configured devices
             date_string = saving_license(table = conf_table)
 
-            # TODO: UNCOMMENT IT
             # printing table with licenses to console
             print(conf_table)
 
@@ -380,10 +384,10 @@ while running_flag:
 
             # TODO: UNCOMMENT CHECKING COM CONNECTION
             # checking ip address is correctly set
-            while ip_flag == False:
-                ip_set = checking_ip_address(lang_dict = lang_expressions)
-                if ip_set == True:
-                    ip_flag = True
+            # while ip_flag == False:
+            #     ip_set = checking_ip_address(lang_dict = lang_expressions)
+            #     if ip_set == True:
+            #         ip_flag = True
 
             print(decorator_1)
 
@@ -458,11 +462,6 @@ while running_flag:
             print("................................")
             print(decorator_1)
 
-            # print('czekanko')
-            # for i in range(0,300):
-            #     sleep(1)
-            #     print('.', end='')
-
             # TODO: NEED TO WORK ABOUT IT HERE
 
             # loop to send commands to every ip address and device
@@ -496,5 +495,5 @@ while running_flag:
 # LAST COMMANDS IN SCRIPT
 # deleting all user-configuration files created while the script was running
 print(decorator_1)
-deleting_conf(lang_dict = lang_expressions)
+# deleting_conf(lang_dict = lang_expressions)
 
